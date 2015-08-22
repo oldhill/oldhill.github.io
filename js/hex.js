a1b0703c1cb9094c73047cb75ad8e5c260f1bf56
oldhill <oldhillcode@gmail.com>
2015-08-22 14:57:57 -0700

    Add hex converter

---
+++ js/hex.js
@@ -0,0 +1,23 @@
+// Converts input base-10 numbers to hex. Requires jQuery.
+
+// TODO make this work with "enter" keypress as well as clicking submit button
+
+$(document).on('ready', function() {
+
+  $('#submitToHex').click(function() {
+
+    // Clear results
+    $('#tohex-results').empty();
+
+    // Grab input and set default to '15'
+    var input = $("#inputNumber").val()
+    if (input === "") {
+      input = '15';
+    }
+
+    // Parse input string into a (base 10) integer, then convert this into a
+    // hex (base 16) representation of itself. Also prepend that "0x" bc why not.
+    $("#tohex-results").append('<br>0x' + parseInt(input).toString(16) + '</br>');
+
+  })
+})
