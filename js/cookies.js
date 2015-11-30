cae22a3ee38ce0109350fb3d65e4fc389556885f
oldhill <oldhillcode@gmail.com>
2015-11-29 23:10:27 -0800

    Correct welcome message

--- js/cookies.js
+++ js/cookies.js
@@ -4,7 +4,7 @@
 $(document).on('ready', function() {
 
   if (document.cookie === '') {
-    $('body').append('Welcome for the first time!');
+    $('body').append('Welcome for the first time in the last five minutes!');
     document.cookie = 'something=somethingelse; secure=True; max-age=300';
   } else {
     $('body').append('Welcome back!');
