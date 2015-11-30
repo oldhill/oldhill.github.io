1b6ace4bfbb59efcbba693db1def015bc78db09f
oldhill <oldhillcode@gmail.com>
2015-11-29 22:14:37 -0800

    Testing in prod bc cookies not served over file protocol

--- js/cookies.js
+++ js/cookies.js
@@ -1,2 +1,9 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Document/cookie
 
+console.log('Any cookies yet? ' + document.cookie);
+
+console.log('Setting a cookie...');
+
+document.cookie = 'something=somethingelse';
+
+console.log('OK set one: ' + document.cookie);
